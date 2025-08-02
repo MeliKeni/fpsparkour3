d7101a63ae0926b9c62ba20e85db9b731a791d72
MeliKeni <melinakenigsberg@gmail.com>
2025-08-02 18:29:03 -0300

    Initial commit

---
+++ Assets/Scripts/InteractionArea.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionArea : MonoBehaviour
+{
+    public GameObject interactionMessage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        interactionMessage.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other){
+        Debug.Log(other.gameObject.name);
+        interactionMessage.SetActive(true);
+    }
+    private void OnTriggerExit(Collider other){
+        interactionMessage.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/MercanciaScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MercanciaScript : MonoBehaviour
+{
+    float cost=1;
+    public MoneyManager moneyManager;
+
+    void Start()
+    {
+        moneyManager = FindObjectOfType<MoneyManager>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            //actualizar dinero por la compra
+            moneyManager.UpdateMoney(+cost);
+            Destroy(gameObject);
+        }        
+    }
+}
