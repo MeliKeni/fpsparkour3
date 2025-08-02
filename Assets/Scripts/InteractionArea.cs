e3ed44db33173adfa6ce3961afacaf48ec941fc1
MeliKeni <melinakenigsberg@gmail.com>
2025-08-02 19:19:17 -0300

    Listo reiniciador de juego
    Falta probarlo, esta programado segun el video

--- Assets/Scripts/InteractionArea.cs
+++ Assets/Scripts/InteractionArea.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class InteractionArea : MonoBehaviour
 {
     public GameObject interactionMessage;
+    public ReiniciarJuego objeto;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,7 +20,11 @@ public class InteractionArea : MonoBehaviour
 
     private void OnTriggerEnter(Collider other){
         Debug.Log(other.gameObject.name);
-        interactionMessage.SetActive(true);
+        objeto = other.GetComponent<ReiniciarJuego>(); //obtengo el script q esta en ese objeto q estoy colisionando, 
+        if(objeto){
+            interactionMessage.SetActive(true);
+
+        }
     }
     private void OnTriggerExit(Collider other){
         interactionMessage.SetActive(false);
---
+++ Assets/Scripts/ReiniciarJuego.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ReiniciarJuego : MonoBehaviour
+{
+        public GameObject interactionArea;
+        
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (Input.GetKeyDown(KeyCode.E) && interactionArea.activeInHierarchy)
+        {
+            SceneManager.LoadScene("FPSParkour");
+        }  
+    }
+
+
+}
